e2f0a9a1a4da2466bbb2d4794ce5f8a4c6fa2b5e
pvntic <lightbulbtion@gmail.com>
2023-09-20 22:29:52 +0900

    ~2023_9_20

---
+++ 05_brute_force/1062.py
@@ -0,0 +1,51 @@
+# 1062번 : 가르침 - Gold 4
+import sys
+input = sys.stdin.readline
+INF = sys.maxsize
+
+"""
+
+"""
+
+n, k = map(int, input().split())
+
+if k < 5:
+    print(0)
+    exit()
+elif k == 26:
+    print(n)
+    exit()
+
+answer = 0
+words = [set(input().rstrip()) for _ in range(n)]
+learn = [0] * 26
+
+for c in ('a', 'c', 'i', 'n', 't'):
+    learn[ord(c) - ord('a')] = 1
+
+
+def dfs(idx, cnt):
+    global answer
+
+    if cnt == k - 5:
+        readcnt = 0
+        for word in words:
+            check = True
+            for w in word:
+                if not learn[ord(w) - ord('a')]:
+                    check = False
+                    break
+            if check:
+                readcnt += 1
+        answer = max(answer, readcnt)
+        return
+
+    for i in range(idx, 26):
+        if not learn[i]:
+            learn[i] = True
+            dfs(i, cnt + 1)
+            learn[i] = False
+
+
+dfs(0, 0)
+print(answer)
