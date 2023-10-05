805293824bc9873102574576374c7c7f22002fbe
pvntic <lightbulbtion@gmail.com>
2023-10-05 11:43:29 +0900

    ~2023_10_05

--- 09_DFS_BFS/3197.py
+++ 09_DFS_BFS/3197.py
@@ -1,5 +1,6 @@
 # 3197번 : 백조의 호수 - Platinum 5
 import sys
+from collections import deque
 input = sys.stdin.readline
 INF = sys.maxsize
 """
@@ -10,6 +11,7 @@ def find(x1, y1):
         return [x1, y1]
     parent[x1][y1] = find(parent[x1][y1][0], parent[x1][y1][1])
     return parent[x1][y1]
+
 def union(x1,y1, x2, y2):
     px1, py1 = find(x1, y1)
     px2, py2 = find(x2, y2)
@@ -32,11 +34,10 @@ def union(x1,y1, x2, y2):
 
 r,c = map(int, input().split())
 board = [list(input().rstrip()) for _ in range(r)]
-que = []
-que1= []
+que = deque()
+que1= deque()
 parent = [[[-1,-1] for _ in range(c)] for _ in range(r)]
-visited = [[False for _ in range(c)] for _ in range(r)]
-visited1 = [[0 for _ in range(c)] for _ in range(r)]
+time = [[0 for _ in range(c)] for _ in range(r)]
 swans = []
 
 dx = [0,0,1,-1]
@@ -44,15 +45,20 @@ dy = [1,-1,0,0]
 
 for i in range(r):
     for j in range(c):
-        if (board[i][j] == '.' or board[i][j] == 'L'):
-            que1.append((i,j))
-            if not visited[i][j]:
-                if board[i][j] == 'L':
-                    swans.append((nx,ny))
-                visited[i][j] = True
+        if board[i][j] == "L":
+            swans.append((i, j))
+            board[i][j] = "."
+            if len(swans) == 2:
+                break
+
+for i in range(r):
+    for j in range(c):
+        if board[i][j] == '.' and time[i][j] == 0:
+                time[i][j] = 1
                 que.append((i,j))
                 while que:
-                    x, y = que.pop(0)
+                    x, y = que.popleft()
+                    union(i,j,x,y)
                     for k in range(4):
                         nx = x + dx[k]
                         ny = y + dy[k]
@@ -60,22 +66,35 @@ for i in range(r):
                         if not (0 <= nx < r and 0 <= ny < c):
                             continue
 
-                        if (board[nx][ny] == '.' or board[nx][ny] == 'L') and not visited[nx][ny]:
-                            if board[nx][ny] == 'L':
-                                swans.append((nx, ny))
-                            visited[nx][ny] = True
+                        if board[nx][ny] == 'X' and time[nx][ny] == 0:
+                            time[nx][ny] = 1
+                            que1.append((nx, ny))
+
+                        if board[nx][ny] == '.' and time[nx][ny] == 0:
+                            time[nx][ny] = 1
                             que.append((nx, ny))
-                            union(i,j,nx,ny)
 
-while que1:
-    x,y = que1.pop(0)
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
 
-        if not (0<=nx<r and 0<=ny<c):
-            continue
+day = 0
 
-        if board[nx][ny] == 'X':
-            que1.append((nx,ny))
-            board[nx][ny] = '.'
+while find(swans[0][0], swans[0][1]) != find(swans[1][0], swans[1][1]):
+    tmp = deque()
+    while que1:
+        x,y  = que1.popleft()
+        board[x][y] = "."
+        merge_point = []
+        for k in range(4):
+            ny, nx = y + dy[k], x + dx[k]
+
+            if not (0 <= nx < r and 0 <= ny < c):
+                continue
+
+            if time[nx][ny] == 0 and board[nx][ny] == "X":
+                time[nx][ny] = 1
+                tmp.append((nx, ny))
+            elif board[nx][ny] == ".":
+                union(nx,ny,x,y)
+
+    que1 = tmp
+    day += 1
+print(day)
